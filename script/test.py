c85b4783440d6dc914fe955bc1a1b0f7fcbb1ec5
pattylo <19451945as@gmail.com>
2022-01-26 13:53:56 +0800

    1326

--- script/test.py
+++ script/test.py
@@ -1,10 +1,31 @@
 #! /usr/bin/env python3.6
 import os
 
-path = '/home/patty/darknet/data/obj'
+if os.path.exists('/home/patty/python/files/train.txt'):
+    os.remove('/home/patty/python/files/train.txt')
 
 imagefiles = []
-os.chdir(path)
+os.chdir('/home/patty/Downloads/Dataset/train')
+
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith("jpg"):
+        imagefiles.append(filename)
+        print (filename)
+    
+os.chdir('/home/patty/python/files')
+with open("train.txt", "w") as generate:
+    for what in imagefiles:
+        generate.write("data/obj/" + what)
+        generate.write("\n")
+    generate.close()
+
+
+if os.path.exists('/home/patty/python/files/test.txt'):
+    os.remove('/home/patty/python/files/test.txt')
+
+imagefiles.clear()
+imagefiles = []
+os.chdir('/home/patty/Downloads/Dataset/test')
 
 for filename in os.listdir(os.getcwd()):
     if filename.endswith("jpg"):
@@ -14,7 +35,7 @@ for filename in os.listdir(os.getcwd()):
 os.chdir('/home/patty/python/files')
 with open("test.txt", "w") as generate:
     for what in imagefiles:
-        generate.write(what)
+        generate.write("data/obj/" + what)
         generate.write("\n")
     generate.close()
 
